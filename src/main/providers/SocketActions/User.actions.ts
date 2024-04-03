eb634eafc594dbcbbf6f703b4a2828d01bed0235
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-04-03 17:13:05 -0500

    docs: Agrega nuevo diagrama en el readme

--- src/main/providers/SocketActions/User.actions.ts
+++ src/main/providers/SocketActions/User.actions.ts
@@ -1,13 +1,14 @@
 import { Server as SocketServer } from "socket.io";
 
 export const registerUserSocketActions = (io: SocketServer) => {
-    io.on("connection", (socket) => {
-        console.log("a user connected");
-        socket.on("disconnect", () => {
-            console.log("user disconnected");
-        });
-        socket.on("subscribe", (msg) => {
-            console.log("message: " + msg);
-        });
+  io.on("connection", (socket) => {
+    // const { token, userAppId } = socket.handshake.query;
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
+    socket.on("subscribe", (msg) => {
+      console.log("message: " + msg);
+    });
+  });
 };
