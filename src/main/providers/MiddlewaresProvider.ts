906f92245c0e572803eedd8e270a69a9926b724b
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-05-28 21:21:37 -0500

    chore: agrega nuevos clg

--- src/main/providers/MiddlewaresProvider.ts
+++ src/main/providers/MiddlewaresProvider.ts
@@ -8,14 +8,14 @@ import morgan from "morgan";
 export const HttpMiddlewareProvider =
   (app: Application, _: ILogger) => (): Application => {
     app.all("/*", function (_, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header("access-control-allow-origin", "*");
       res.header(
-        "Access-Control-Allow-Headers",
-        "Content-Type,accept,access_token,X-Requested-With"
+        "access-control-allow-headers",
+        "content-type,accept,access_token,X-Requested-With"
       );
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.header("Access-Control-Allow-Credentials", "true");
-      res.header("Access-Control-Expose-Headers", "Content-Length");
+      res.header("Access-control-allow-methods", "GET, POST, PUT, DELETE");
+      res.header("Access-control-allow-credentials", "true");
+      res.header("Access-control-expose-headers", "Content-Length");
       next();
     });
 
